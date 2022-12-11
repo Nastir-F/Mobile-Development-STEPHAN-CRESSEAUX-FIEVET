01a5ce29064bb8441084de488a8fb9fa2762273b
Tristan FIEVET <tristan.fievet@efrei.net>
2022-12-11 17:29:36 +0000

    Initial Commit

---
+++ Mobile Development/Mobile_DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_DevelopmentApp.swift
+//  Mobile Development
+//
+//  Created by user231371 on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
