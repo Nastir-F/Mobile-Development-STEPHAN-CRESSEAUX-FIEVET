17625b65650b4bb2b126fb1a73270ea6c417c87c
chloestephan <chloe.stephan@efrei.net>
2023-01-06 15:47:36 +0000

    clean code

--- Mobile Development/Views/HomeView.swift
+++ Mobile Development/Views/HomeView.swift
@@ -13,18 +13,15 @@ struct HomeView: View {
     var body: some View {
             
             VStack {
-            
                 // *** header of the app ***
                 HeaderView()
                 
                 // *** body of the app ***
-                
                 EventsDisplayView()
             
-            // *** footer of the app ***
-            FooterView()
+                // *** footer of the app ***
+                FooterView()
         }
-        Spacer()
     }
 }
 
